829b7e1926bec50c9cdf9c16d3fecac04e71bc74
Camp Akers <campbell.akers@mcusd709.org>
2024-08-21 09:08:24 -0500

    tonight the music seems so loud

---
+++ AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe{
+   public static void main(String[] args){
+    System.out.println("Hi there! My name is Camp.");
+     System.out.println("I am a junior at Mortoh High school.");
+      System.out.println("My hobbies include annoying Mr. Zeller, playing soccer, and listening to annoying music at top volume! Thanks for reading!");
+   } 
+}
